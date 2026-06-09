16574a6580785b073f0a1f14ae61c9c64c75c2fb
ysh <mysh212@gmail.com>
2026-06-09 10:43:36 +0800

    Implement vote

--- lib/test.py
+++ lib/test.py
@@ -1,7 +1,7 @@
 from core.general import *
 from core.log import log as _log
 
-from lib.env import BATCH_SIZE, TTA_TIMES, device
+from lib.env import BATCH_SIZE, TTA_TIMES, VOTE, device
 from lib.type import ts
 
 import torch
@@ -15,6 +15,28 @@ f = DataLoader(_f, batch_size = BATCH_SIZE)
 
 log = _log('test')
 
+models = None
+
+def predict(model, imgs):
+    global models
+    if not VOTE:
+        return model(imgs)
+
+    ans = None
+    if models is None:
+        models = [[i, torch.load(f'final/base/{i}')] for i in ls('final/base') if i.endswith('.mdl')]
+        for name, model in models:
+            model.load_state_dict(torch.load(f'final/var/{name}'))
+
+    for name, model in models:
+        debug(f'predicting {name}')
+        if ans is None:
+            ans = model(imgs)
+        else:
+            ans += model(imgs)
+
+    return ans
+
 def run_tests(model) -> dict:
     aans = {}
     log.info('Run Tests')(f'Start running tests')
@@ -22,10 +44,10 @@ def run_tests(model) -> dict:
         img = img.permute(0, 3, 1, 2).contiguous()
         img = img.to(device)
 
-        ans = model(img)
+        ans = predict(model, img)
         for _ in range(TTA_TIMES):
             nimg = _f.batch_varint(img)
-            ans += model(nimg)
+            ans += predict(model, nimg)
 
         # pre = pre / (TTA_TIMES + 1)
         for i, j in zip(path, torch.argmax(ans, dim = 1)):
